6b35298bea734a039e38ea6f83f141da7df630e2
afsheen minnotte <afsheenminnotte@gmail.com>
2023-11-27 13:44:58 +0400

    to debug

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy] # DRY
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    # @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+    @categories = Category.all
+  end
+
+  def create
+   @article = Article.new(article_params)
+   @article.save
+   redirect_to articles_path(@article), notice: 'To lartik in bien creer.'
+
+  end
+
+  def edit
+    # @article = Article.find(params[:id])
+  end
+
+  def update
+    # @article = Article
+    @article = Article.update(article_params)
+    redirect_to article_path(@article), notice: 'To lartik in bien update.'
+  end
+
+  def destroy
+    # @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: 'To lartik in bien refasser.'
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:name, :description, :price, :quantity, :category_id)
+  end
+
+  def set_article
+   @article = Article.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "about", to: "pages#about"
-  
+
 #category routes
   get "categories", to: "categories#index"
   get "categories/new", to: "categories#new"
@@ -10,6 +10,15 @@ Rails.application.routes.draw do
   get "categories/:id", to: "categories#show", as: :category
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+#article routes
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+  get "articles/:id", to: "articles#show", as: :article
+  get "articles/:id/edit", to: "articles#edit"
+  patch "articles/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#destroy"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
